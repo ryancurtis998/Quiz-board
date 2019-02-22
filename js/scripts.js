19e77ab657a248b8a2454ee66b8b1da4af142f04
santa <ryancurtis998@yahoo.com>
2019-02-22 20:16:02 +0300

    adding of js.script

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
-  $(input[type=button]).click(function(){
-    var question1 = input[name=question1].val()
-  });
-});
+$(document).ready(function(){
+   $("input[type='button']").click(function(){
+       var quesone = parseInt($("input[name='question1']:checked").val());
+       var questwo = parseInt($("input[name='question2']:checked").val());
+       var questhree = parseInt($("input[name='question3']:checked").val());
+       var quesfour = parseInt($("input[name='question4']:checked").val());
+       var quesfive = parseInt($("input[name='question5']:checked").val());
+       var quessix = parseInt($("input[name='question6']:checked").val());
+       var quesseven = parseInt($("input[name='question7']:checked").val());
+       var queseightt = parseInt($("input[name='question8']:checked").val());
+       var quesnine = parseInt($("input[name='question9']:checked").val());
+       var questen = parseInt($("input[name='question10']:checked").val());
+       var total = quesone + questwo + questhree + quesfour + quesfive + quessix + quesseven + queseight + quesnine + q ;
+       document.getElementById('output').innerHTML="you have scored " + total;
+   });
+ });
